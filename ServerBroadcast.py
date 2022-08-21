aeb06fc2c7dff4e6064c1dd5ea6b0ad07d024c89
Decapitated <brettzkyjo10@gmail.com>
2022-08-21 00:25:39 -0500

    Change vocabulary. Plus some refacotring.

--- ServerBroadcast.py
+++ ServerBroadcast.py
@@ -3,9 +3,7 @@ from time import sleep
 import socket
 import utilities
 
-class MulticastServer:
-    __multicasting = False
-    __connecting = False
+class ServerBroadcast:
     client = None
 
     def __init__(self, udp_group: str, udp_port: int, tcp_port: int, secret: str):
@@ -13,11 +11,6 @@ class MulticastServer:
         self.UDP_PORT = udp_port
         self.TCP_PORT = tcp_port
         self.SECRET = secret
-
-    def close(self):
-        self.__multicasting = False
-        self.__connecting = False
-        self.client.close()
 
     def startMulticast(self) -> Thread:
         thread = Thread(target=self.__multicast)
@@ -30,25 +23,23 @@ class MulticastServer:
         return thread
 
     def __multicast(self):
-        self.__multicasting = True
         # Setup UDP socket
         udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         # Set up socket to send multicast
         udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
         LOCAL_IP = utilities.getLocalIp()
         udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(LOCAL_IP))
-        while self.__multicasting:
+        while self.client == None:
             udp_socket.sendto(":".join([self.SECRET, str(self.TCP_PORT)]).encode(), (self.UDP_GROUP, self.UDP_PORT))
             sleep(1)
         udp_socket.close()
 
     def __awaitConnect(self):
-        self.__connecting = True
         tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Use 0.0.0.0 for tcp or connection will be refused.
         tcp_socket.bind(("0.0.0.0", self.TCP_PORT))
         tcp_socket.listen()
-        while self.__connecting:
+        while self.client == None:
             tempClient = tcp_socket.accept()[0]
             data = tempClient.recv(1024).decode()
             if(data != self.SECRET):
@@ -56,7 +47,5 @@ class MulticastServer:
                 continue
             else:
                 print("Client Connected!")
-                self.__multicasting = False
-                self.__connecting = False
                 self.client = tempClient
         tcp_socket.close()
--- ServerFinder.py
+++ ServerFinder.py
@@ -1,8 +1,8 @@
 from ast import Pass
-from MulticastServer import MulticastServer
+from ServerBroadcast import ServerBroadcast
 
 def broadcastServer(udp_group: str, udp_port: int, tcp_port: int, secret: str):
-    server = MulticastServer(udp_group, udp_port, tcp_port, secret)
+    server = ServerBroadcast(udp_group, udp_port, tcp_port, secret)
     awaitClient = server.startAwait()
     server.startMulticast()
     awaitClient.join()
